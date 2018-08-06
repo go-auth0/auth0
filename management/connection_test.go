d9cdad7c970d7f81e1716aaebe59d47c16fd2549
Laurent DUTHEIL <ldutheil@octo.com>
2018-08-06 16:19:44 +0200

    add test for CustomScripts and Configuration

--- management/connection_test.go
+++ management/connection_test.go
@@ -99,6 +99,44 @@ func TestConnection(t *testing.T) {
 		t.Logf("%v\n", c)
 	})
 
+	t.Run("Update custom scripts and configuration values", func(t *testing.T) {
+		id := c.ID
+		c.ID = ""       // read-only
+		c.Name = ""     // read-only
+		c.Strategy = "" // read-only
+		c.Options.CustomScripts = map[string]interface{}{
+			"get_user": "function(email, callback) { return callback(null) }",
+		}
+		c.Options.Configuration = map[string]interface{}{
+			"KEY_1": "VALUE_1",
+			"KEY_2": "VALUE_2",
+		}
+
+		err = m.Connection.Update(id, c)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if c.Options.CustomScripts["get_user"] != "function(email, callback) { return callback(null) }" {
+			t.Fatal("response should contain a custom script with the key 'get_user'")
+		}
+		if value, exist := c.Options.Configuration["KEY_1"]; exist {
+			if value == "VALUE_1" {
+				t.Fatal("response should contain a CRYPTED configuration value for 'KEY_1'")
+			}
+		} else {
+			t.Fatal("response should contain a configuration key 'KEY_1'")
+		}
+		if value, exist := c.Options.Configuration["KEY_2"]; exist {
+			if value == "VALUE_1" {
+				t.Fatal("response should contain a CRYPTED configuration value for 'KEY_2'")
+			}
+		} else {
+			t.Fatal("response should contain a configuration key 'KEY_2'")
+		}
+
+	})
+
 	t.Run("Delete", func(t *testing.T) {
 		err = m.Connection.Delete(c.ID)
 		if err != nil {
