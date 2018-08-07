7b9d12c36900ed25c0079d173173dad865f4879d
Laurent DUTHEIL <ldutheil@octo.com>
2018-08-07 09:32:50 +0200

    apply code review

--- management/connection_test.go
+++ management/connection_test.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"testing"
 	"time"
-	"encoding/json"
-	"strings"
 )
 
 func TestConnection(t *testing.T) {
@@ -16,59 +14,6 @@ func TestConnection(t *testing.T) {
 	}
 
 	var err error
-
-	t.Run("Validate Contract", func(t *testing.T) {
-		data := `
-			{
-				"id": "",
-				"name": "",
-				"strategy": "",
-				"options": {
-					"validation": {},
-					"passwordPolicy": "",
-					"password_history": {},
-					"password_no_personal_info": {},
-					"password_dictionary": {},
-					"api_enable_users": true,
-					"basic_profile": true,
-					"ext_admin": true,
-					"ext_is_suspended": true,
-					"ext_agreed_terms": true,
-					"ext_groups": true,
-					"ext_assigned_plans": true,
-					"ext_profile": true,
-					"enabledDatabaseCustomization": true,
-					"brute_force_protection": true,
-					"import_mode": true,
-					"disable_signup": true,
-					"upstream_params": {},
-					"client_id": "",
-					"client_secret": "",
-					"tenant_domain": "",
-					"domain_aliases": [],
-					"use_wsfed": true,
-					"waad_protocol": "",
-					"waad_common_endpoint": true,
-					"app_id": "",
-					"app_domain": "",
-					"custom_scripts": {},
-					"configuration": {}
-				},
-				"enabled_clients": [],
-				"realms": [],
-				"metadata": ""
-			}
-		`
-
-		reader := strings.NewReader(data)
-		decoder := json.NewDecoder(reader)
-		decoder.DisallowUnknownFields()
-
-		var connection Connection
-		if err := decoder.Decode(&connection); err != nil {
-			t.Fatal("Connection should unmarshall valid json:\n", err)
-		}
-	})
 
 	t.Run("Create", func(t *testing.T) {
 		err = m.Connection.Create(c)
@@ -91,50 +36,26 @@ func TestConnection(t *testing.T) {
 		c.ID = ""       // read-only
 		c.Name = ""     // read-only
 		c.Strategy = "" // read-only
+		c.Options = &ConnectionOptions{
+			CustomScripts: map[string]interface{}{"get_user": "function(email, callback) { return callback(null) }"},
+			Configuration: map[string]interface{}{"foo": "bar"},
+		}
+
+		cc := c // make a copy so we can compare later
 
 		err = m.Connection.Update(id, c)
 		if err != nil {
 			t.Error(err)
 		}
 		t.Logf("%v\n", c)
-	})
 
-	t.Run("Update custom scripts and configuration values", func(t *testing.T) {
-		id := c.ID
-		c.ID = ""       // read-only
-		c.Name = ""     // read-only
-		c.Strategy = "" // read-only
-		c.Options.CustomScripts = map[string]interface{}{
-			"get_user": "function(email, callback) { return callback(null) }",
-		}
-		c.Options.Configuration = map[string]interface{}{
-			"KEY_1": "VALUE_1",
-			"KEY_2": "VALUE_2",
+		if c.Options.CustomScripts["get_user"] != cc.Options.CustomScripts["get_user"] {
+			t.Fatal(`unexpected result for "get_user" custom script`)
 		}
 
-		err = m.Connection.Update(id, c)
-		if err != nil {
-			t.Error(err)
+		if _, exist := c.Options.Configuration["foo"]; !exist {
+			t.Fatal(`missing key "foo"`)
 		}
-
-		if c.Options.CustomScripts["get_user"] != "function(email, callback) { return callback(null) }" {
-			t.Fatal("response should contain a custom script with the key 'get_user'")
-		}
-		if value, exist := c.Options.Configuration["KEY_1"]; exist {
-			if value == "VALUE_1" {
-				t.Fatal("response should contain a CRYPTED configuration value for 'KEY_1'")
-			}
-		} else {
-			t.Fatal("response should contain a configuration key 'KEY_1'")
-		}
-		if value, exist := c.Options.Configuration["KEY_2"]; exist {
-			if value == "VALUE_1" {
-				t.Fatal("response should contain a CRYPTED configuration value for 'KEY_2'")
-			}
-		} else {
-			t.Fatal("response should contain a configuration key 'KEY_2'")
-		}
-
 	})
 
 	t.Run("Delete", func(t *testing.T) {
