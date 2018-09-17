dac684b55816a151e0e91a2d6407d2094781d9ea
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2018-09-17 16:03:07 +0200

    comment out example to debug dep import

--- management/example_test.go
+++ management/example_test.go
@@ -1,37 +1,37 @@
 package management_test
 
-import (
-	"fmt"
-	"os"
+// import (
+// 	"fmt"
+// 	"os"
 
-	auth0 "github.com/yieldr/go-auth0"
-	"github.com/yieldr/go-auth0/management"
-)
+// 	auth0 "github.com/yieldr/go-auth0"
+// 	"github.com/yieldr/go-auth0/management"
+// )
 
-var (
-	domain = os.Getenv("AUTH0_DOMAIN")
-	id     = os.Getenv("AUTH0_CLIENT_ID")
-	secret = os.Getenv("AUTH0_CLIENT_SECRET")
-)
+// var (
+// 	domain = os.Getenv("AUTH0_DOMAIN")
+// 	id     = os.Getenv("AUTH0_CLIENT_ID")
+// 	secret = os.Getenv("AUTH0_CLIENT_SECRET")
+// )
 
-func ExampleUser() {
-	m, err := management.New(domain, id, secret)
-	if err != nil {
-		fmt.Printf("Failed creating management client. %s", err)
-	}
+// func ExampleUser() {
+// 	m, err := management.New(domain, id, secret)
+// 	if err != nil {
+// 		fmt.Printf("Failed creating management client. %s", err)
+// 	}
 
-	u := &management.User{
-		Connection: auth0.String("Username-Password-Authentication"),
-		Email:      auth0.String("smith@example.com"),
-		Password:   auth0.String("F4e3DA1a6cDD"),
-	}
+// 	u := &management.User{
+// 		Connection: auth0.String("Username-Password-Authentication"),
+// 		Email:      auth0.String("smith@example.com"),
+// 		Password:   auth0.String("F4e3DA1a6cDD"),
+// 	}
 
-	err = m.User.Create(u)
-	if err != nil {
-		fmt.Printf("Failed creating user. %s", err)
-	}
-	defer m.User.Delete(auth0.StringValue(u.ID))
+// 	err = m.User.Create(u)
+// 	if err != nil {
+// 		fmt.Printf("Failed creating user. %s", err)
+// 	}
+// 	defer m.User.Delete(auth0.StringValue(u.ID))
 
-	fmt.Printf("User created!")
-	// Output: User created!
-}
+// 	fmt.Printf("User created!")
+// 	// Output: User created!
+// }
