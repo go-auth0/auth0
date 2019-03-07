6cc4181ba7a31eec04ba097eed938298629811c2
Simon Barendse <simon.barendse@gmail.com>
2019-03-07 13:29:05 +0100

    Add comments to the fields of the job struct
    These are the descriptions these fields have in the api docs.

--- management/job.go
+++ management/job.go
@@ -3,12 +3,18 @@ package management
 import "time"
 
 type Job struct {
-	ID        *string    `json:"id,omitempty"`
-	Status    *string    `json:"status,omitempty"`
-	Type      *string    `json:"type,omitempty"`
+	// The job's identifier. Useful to retrieve its status
+	ID *string `json:"id,omitempty"`
+	// The job's status
+	Status *string `json:"status,omitempty"`
+	// The type of job
+	Type *string `json:"type,omitempty"`
+	// The date when the job was created.
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 
-	UserID   *string `json:"user_id,omitempty"`
+	// The user_id of the user to whom the email will be sent
+	UserID *string `json:"user_id,omitempty"`
+	// The id of the client, if not provided the global one will be used
 	ClientID *string `json:"cliend_id,omitempty"`
 }
 
