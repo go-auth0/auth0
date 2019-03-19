889b614e271714974dbafb4dcdaa9048026c252f
Ron Barber <rbarber@luc.id>
2019-03-19 16:23:00 -0500

    Fix test.  Create a user before making request to change password

--- management/dbconnections_test.go
+++ management/dbconnections_test.go
@@ -8,8 +8,23 @@ import (
 
 func TestChangePassword(t *testing.T) {
 
+	u := &User{
+		Connection: auth0.String("Username-Password-Authentication"),
+		Email:      auth0.String("chuck@chucknorris.com"),
+		Password:   auth0.String("5301111b-b31b-47c4-bf3d-0c26ea57bdf4"),
+	}
+
+	var err error
+
+	t.Run("Create", func(t *testing.T) {
+		err = m.User.Create(u)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("%v\n", u)
+	})
+
 	changePw := &DBConnectionsChangePassword{
-		ClientID:   auth0.String("Auth0 clientID"),
 		Connection: auth0.String("Username-Password-Authentication"),
 		Email:      auth0.String("chuck@chucknorris.com"),
 	}
@@ -20,5 +35,12 @@ func TestChangePassword(t *testing.T) {
 			t.Fatal(err)
 		}
 		t.Logf("%v\n", str)
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		err = m.User.Delete(auth0.StringValue(u.ID))
+		if err != nil {
+			t.Error(err)
+		}
 	})
 }
