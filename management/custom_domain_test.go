ab719fe08b8a812c91d37521e0463336e415b2b3
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2019-11-18 20:19:46 +0100

    remove custom domain update

--- management/custom_domain_test.go
+++ management/custom_domain_test.go
@@ -41,27 +41,14 @@ func TestCustomDomain(t *testing.T) {
 		t.Logf("%v\n", c)
 	})
 
-	t.Run("Update", func(t *testing.T) {
-		id := auth0.StringValue(c.ID)
-
-		c.ID = nil // read-only
-		c.Domain = auth0.String("id.example.com")
-
-		err = m.CustomDomain.Update(id, c)
-		if err, ok := err.(Error); ok && err.Status() == http.StatusNotFound {
-			t.Skip(err)
-		} else {
-			t.Error(err)
-		}
-		t.Logf("%v\n", c)
-	})
-
 	t.Run("Delete", func(t *testing.T) {
 		err = m.CustomDomain.Delete(auth0.StringValue(c.ID))
-		if err, ok := err.(Error); ok && err.Status() == http.StatusNotFound {
-			t.Skip(err)
-		} else {
-			t.Error(err)
+		if err != nil {
+			if err, ok := err.(Error); ok && err.Status() == http.StatusNotFound {
+				t.Skip(err)
+			} else {
+				t.Error(err)
+			}
 		}
 	})
 }
