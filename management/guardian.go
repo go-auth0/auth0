dc99f3850d3c5c81c6abfc8c5b71a46b441f1d64
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2019-11-26 12:33:48 +0100

    improving on the work of bishtawi

--- management/guardian.go
+++ management/guardian.go
@@ -1,21 +1,8 @@
 package management
 
-const (
-	DuoGuardianFactor              GuardianFactorType = "duo"
-	EmailGuardianFactor            GuardianFactorType = "email"
-	OtpGuardianFactor              GuardianFactorType = "otp"
-	PushNotificationGuardianFactor GuardianFactorType = "push-notification"
-	SmsGuardianFactor              GuardianFactorType = "sms"
-)
+import "encoding/json"
 
-type GuardianFactorType string
-
-type GuardianFactor struct {
-	// States if this factor is enabled
-	Enabled *bool `json:"enabled,omitempty"`
-}
-
-type GuardianFactorStatus struct {
+type MultiFactor struct {
 	// States if this factor is enabled
 	Enabled *bool `json:"enabled,omitempty"`
 
@@ -26,7 +13,12 @@ type GuardianFactorStatus struct {
 	TrialExpired *bool `json:"trial_expired,omitempty"`
 }
 
-type GuardianSmsTemplate struct {
+func (mfa *MultiFactor) String() string {
+	b, _ := json.MarshalIndent(mfa, "", "  ")
+	return string(b)
+}
+
+type MultiFactorSMSTemplate struct {
 	// Message sent to the user when they are invited to enroll with a phone number
 	EnrollmentMessage *string `json:"enrollment_message,omitempty"`
 
@@ -34,24 +26,34 @@ type GuardianSmsTemplate struct {
 	VerificationMessage *string `json:"verification_message,omitempty"`
 }
 
-type GuardianPushNotificationSnsConfig struct {
-	// AWS Access Key ID
-	AwsAccessKeyID *string `json:"aws_access_key_id,omitempty"`
-
-	// AWS Secret Access Key ID
-	AwsSecretAccessKeyID *string `json:"aws_secret_access_key,omitempty"`
-
-	// AWS Region
-	AwsRegion *string `json:"aws_region,omitempty"`
-
-	// SNS APNS Platform Application ARN
-	SnsApnsPlatformApplicationArn *string `json:"sns_apns_platform_application_arn,omitempty"`
-
-	// SNS GCM Platform Application ARN
-	SnsGcmPlatformApplicationArn *string `json:"sns_gcm_platform_application_arn,omitempty"`
+func (sms *MultiFactorSMSTemplate) String() string {
+	b, _ := json.MarshalIndent(sms, "", "  ")
+	return string(b)
 }
 
-type GuardianSmsTwilioConfig struct {
+type MultiFactorProviderAmazonSNS struct {
+	// AWS Access Key ID
+	AccessKeyID *string `json:"aws_access_key_id,omitempty"`
+
+	// AWS Secret Access Key ID
+	SecretAccessKeyID *string `json:"aws_secret_access_key,omitempty"`
+
+	// AWS Region
+	Region *string `json:"aws_region,omitempty"`
+
+	// SNS APNS Platform Application ARN
+	APNSPlatformApplicationARN *string `json:"sns_apns_platform_application_arn,omitempty"`
+
+	// SNS GCM Platform Application ARN
+	GCMPlatformApplicationARN *string `json:"sns_gcm_platform_application_arn,omitempty"`
+}
+
+func (sns *MultiFactorProviderAmazonSNS) String() string {
+	b, _ := json.MarshalIndent(sns, "", "  ")
+	return string(b)
+}
+
+type MultiFactorProviderTwilio struct {
 	// From number
 	From *string `json:"from,omitempty"`
 
@@ -62,53 +64,112 @@ type GuardianSmsTwilioConfig struct {
 	AuthToken *string `json:"auth_token,omitempty"`
 
 	// Twilio SID
-	Sid *string `json:"sid,omitempty"`
+	SID *string `json:"sid,omitempty"`
+}
+
+func (twilio *MultiFactorProviderTwilio) String() string {
+	b, _ := json.MarshalIndent(twilio, "", "  ")
+	return string(b)
 }
 
 type GuardianManager struct {
-	m *Management
+	MultiFactor *MultiFactorManager
 }
 
 func NewGuardianManager(m *Management) *GuardianManager {
-	return &GuardianManager{m}
+	return &GuardianManager{
+		&MultiFactorManager{m,
+			&MultiFactorSMS{m},
+			&MultiFactorPush{m},
+			&MultiFactorEmail{m},
+			&MultiFactorDUO{m},
+			&MultiFactorOTP{m},
+		},
+	}
 }
 
-func (gm *GuardianManager) ListFactors() ([]*GuardianFactorStatus, error) {
-	var gf []*GuardianFactorStatus
-	err := gm.m.get(gm.m.uri("guardian/factors"), &gf)
-	return gf, err
+type MultiFactorManager struct {
+	m *Management
+
+	SMS   *MultiFactorSMS
+	Push  *MultiFactorPush
+	Email *MultiFactorEmail
+	DUO   *MultiFactorDUO
+	OTP   *MultiFactorOTP
 }
 
-func (gm *GuardianManager) UpdateFactor(name GuardianFactorType, gf *GuardianFactor) error {
-	return gm.m.put(gm.m.uri("guardian/factors", string(name)), gf)
+func (mfm *MultiFactorManager) List(opts ...reqOption) ([]*MultiFactor, error) {
+	var mf []*MultiFactor
+	err := mfm.m.get(mfm.m.uri("guardian", "factors")+mfm.m.q(opts), &mf)
+	return mf, err
 }
 
-func (gm *GuardianManager) GetSmsTemplate() (*GuardianSmsTemplate, error) {
-	st := new(GuardianSmsTemplate)
-	err := gm.m.get(gm.m.uri("guardian/factors/sms/templates"), st)
-	return st, err
+type MultiFactorSMS struct{ m *Management }
+
+func (sm *MultiFactorSMS) Enable(enabled bool) error {
+	return sm.m.put(sm.m.uri("guardian", "factors", "sms"), &MultiFactor{
+		Enabled: &enabled,
+	})
 }
 
-func (gm *GuardianManager) UpdateSmsTemplate(st *GuardianSmsTemplate) error {
-	return gm.m.put(gm.m.uri("guardian/factors/sms/templates"), st)
+func (sm *MultiFactorSMS) Template() (*MultiFactorSMSTemplate, error) {
+	t := new(MultiFactorSMSTemplate)
+	err := sm.m.get(sm.m.uri("guardian", "factors", "sms", "templates"), t)
+	return t, err
 }
 
-func (gm *GuardianManager) GetPushNotificationSnsConfig() (*GuardianPushNotificationSnsConfig, error) {
-	sc := new(GuardianPushNotificationSnsConfig)
-	err := gm.m.get(gm.m.uri("guardian/factors/push-notification/providers/sns"), sc)
-	return sc, err
+func (sm *MultiFactorSMS) UpdateTemplate(st *MultiFactorSMSTemplate) error {
+	return sm.m.put(sm.m.uri("guardian", "factors", "sms", "templates"), st)
 }
 
-func (gm *GuardianManager) UpdatePushNotificationSnsConfig(sc *GuardianPushNotificationSnsConfig) error {
-	return gm.m.put(gm.m.uri("guardian/factors/push-notification/providers/sns"), sc)
-}
-
-func (gm *GuardianManager) GetSmsTwilioConfig() (*GuardianSmsTwilioConfig, error) {
-	tc := new(GuardianSmsTwilioConfig)
-	err := gm.m.get(gm.m.uri("guardian/factors/sms/providers/twilio"), tc)
+func (sm *MultiFactorSMS) Twilio() (*MultiFactorProviderTwilio, error) {
+	tc := new(MultiFactorProviderTwilio)
+	err := sm.m.get(sm.m.uri("guardian", "factors", "sms", "providers", "twilio"), tc)
 	return tc, err
 }
 
-func (gm *GuardianManager) UpdateSmsTwilioConfig(tc *GuardianSmsTwilioConfig) error {
-	return gm.m.put(gm.m.uri("guardian/factors/sms/providers/twilio"), tc)
+func (sm *MultiFactorSMS) UpdateTwilio(twilio *MultiFactorProviderTwilio) error {
+	return sm.m.put(sm.m.uri("guardian", "factors", "sms", "providers", "twilio"), twilio)
+}
+
+type MultiFactorPush struct{ m *Management }
+
+func (pm *MultiFactorPush) Enable(enabled bool) error {
+	return pm.m.put(pm.m.uri("guardian", "factors", "push-notification"), &MultiFactor{
+		Enabled: &enabled,
+	})
+}
+
+func (pm *MultiFactorPush) AmazonSNS() (*MultiFactorProviderAmazonSNS, error) {
+	sc := new(MultiFactorProviderAmazonSNS)
+	err := pm.m.get(pm.m.uri("guardian", "factors", "push-notification", "providers", "sns"), sc)
+	return sc, err
+}
+
+func (pm *MultiFactorPush) UpdateAmazonSNS(sc *MultiFactorProviderAmazonSNS) error {
+	return pm.m.put(pm.m.uri("guardian", "factors", "push-notification", "providers", "sns"), sc)
+}
+
+type MultiFactorEmail struct{ m *Management }
+
+func (em *MultiFactorEmail) Enable(enabled bool) error {
+	return em.m.put(em.m.uri("guardian", "factors", "email"), &MultiFactor{
+		Enabled: &enabled,
+	})
+}
+
+type MultiFactorDUO struct{ m *Management }
+
+func (em *MultiFactorDUO) Enable(enabled bool) error {
+	return em.m.put(em.m.uri("guardian", "factors", "duo"), &MultiFactor{
+		Enabled: &enabled,
+	})
+}
+
+type MultiFactorOTP struct{ m *Management }
+
+func (em *MultiFactorOTP) Enable(enabled bool) error {
+	return em.m.put(em.m.uri("guardian", "factors", "otp"), &MultiFactor{
+		Enabled: &enabled,
+	})
 }
--- management/guardian_test.go
+++ management/guardian_test.go
@@ -7,112 +7,161 @@ import (
 )
 
 func TestGuardian(t *testing.T) {
-	testFactor := OtpGuardianFactor
 
-	defer func() {
-		emptyString := ""
-		m.Guardian.UpdateFactor(testFactor, &GuardianFactor{
-			Enabled: auth0.Bool(false),
+	t.Run("MultiFactor", func(t *testing.T) {
+
+		t.Run("List", func(t *testing.T) {
+			mfa, err := m.Guardian.MultiFactor.List()
+			if err != nil {
+				t.Error(err)
+			}
+			t.Logf("%v\n", mfa)
 		})
-		m.Guardian.UpdateSmsTemplate(&GuardianSmsTemplate{
-			EnrollmentMessage:   &emptyString,
-			VerificationMessage: &emptyString,
+
+		t.Run("SMS", func(t *testing.T) {
+
+			t.Run("Enable", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.SMS.Enable(false)
+
+				err := m.Guardian.MultiFactor.SMS.Enable(true)
+				if err != nil {
+					t.Error(err)
+				}
+
+				mfa, _ := m.Guardian.MultiFactor.List()
+				t.Logf("%v\n", mfa)
+			})
+
+			t.Run("Template", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.SMS.UpdateTemplate(&MultiFactorSMSTemplate{
+					EnrollmentMessage:   auth0.String(""),
+					VerificationMessage: auth0.String(""),
+				})
+
+				err := m.Guardian.MultiFactor.SMS.UpdateTemplate(&MultiFactorSMSTemplate{
+					EnrollmentMessage:   auth0.String("Test {{code}} for {{tenant.friendly_name}}"),
+					VerificationMessage: auth0.String("Test {{code}} for {{tenant.friendly_name}}"),
+				})
+				if err != nil {
+					t.Error(err)
+				}
+
+				template, err := m.Guardian.MultiFactor.SMS.Template()
+				if err != nil {
+					t.Error(err)
+				}
+				t.Logf("%v\n", template)
+			})
+
+			t.Run("Twilio", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.SMS.UpdateTwilio(&MultiFactorProviderTwilio{
+					From:      auth0.String(""),
+					AuthToken: auth0.String(""),
+					SID:       auth0.String(""),
+				})
+
+				err := m.Guardian.MultiFactor.SMS.UpdateTwilio(&MultiFactorProviderTwilio{
+					From:      auth0.String("123456789"),
+					AuthToken: auth0.String("test_token"),
+					SID:       auth0.String("test_sid"),
+				})
+				if err != nil {
+					t.Error(err)
+				}
+
+				twilio, err := m.Guardian.MultiFactor.SMS.Twilio()
+				if err != nil {
+					t.Error(err)
+				}
+				t.Logf("%v\n", twilio)
+			})
 		})
-		m.Guardian.UpdatePushNotificationSnsConfig(&GuardianPushNotificationSnsConfig{
-			AwsAccessKeyID:                &emptyString,
-			AwsSecretAccessKeyID:          &emptyString,
-			AwsRegion:                     &emptyString,
-			SnsApnsPlatformApplicationArn: &emptyString,
-			SnsGcmPlatformApplicationArn:  &emptyString,
+
+		t.Run("Push", func(t *testing.T) {
+
+			t.Run("Enable", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.Push.Enable(false)
+
+				err := m.Guardian.MultiFactor.Push.Enable(true)
+				if err != nil {
+					t.Error(err)
+				}
+
+				mfa, _ := m.Guardian.MultiFactor.List()
+				t.Logf("%v\n", mfa)
+			})
+
+			t.Run("AmazonSNS", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.Push.UpdateAmazonSNS(&MultiFactorProviderAmazonSNS{
+					AccessKeyID:                auth0.String(""),
+					SecretAccessKeyID:          auth0.String(""),
+					Region:                     auth0.String(""),
+					APNSPlatformApplicationARN: auth0.String(""),
+					GCMPlatformApplicationARN:  auth0.String(""),
+				})
+
+				err := m.Guardian.MultiFactor.Push.UpdateAmazonSNS(&MultiFactorProviderAmazonSNS{
+					AccessKeyID:                auth0.String("test"),
+					SecretAccessKeyID:          auth0.String("test_secret"),
+					Region:                     auth0.String("us-west-1"),
+					APNSPlatformApplicationARN: auth0.String("test_arn"),
+					GCMPlatformApplicationARN:  auth0.String("test_arn"),
+				})
+				if err != nil {
+					t.Error(err)
+				}
+
+				sns, err := m.Guardian.MultiFactor.Push.AmazonSNS()
+				if err != nil {
+					t.Error(err)
+				}
+				t.Logf("%v\n", sns)
+			})
 		})
-		m.Guardian.UpdateSmsTwilioConfig(&GuardianSmsTwilioConfig{
-			From:                &emptyString,
-			MessagingServiceSid: &emptyString,
-			AuthToken:           &emptyString,
-			Sid:                 &emptyString,
+
+		t.Run("Email", func(t *testing.T) {
+
+			t.Run("Enable", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.Email.Enable(false)
+
+				err := m.Guardian.MultiFactor.Email.Enable(true)
+				if err != nil {
+					t.Error(err)
+				}
+
+				mfa, _ := m.Guardian.MultiFactor.List()
+				t.Logf("%v\n", mfa)
+			})
 		})
-	}()
 
-	t.Run("ListFactors", func(t *testing.T) {
-		gf, err := m.Guardian.ListFactors()
-		if err != nil {
-			t.Error(err)
-		}
-		t.Logf("%v\n", gf)
-	})
+		t.Run("DUO", func(t *testing.T) {
 
-	t.Run("UpdateFactor", func(t *testing.T) {
-		err := m.Guardian.UpdateFactor(testFactor, &GuardianFactor{
-			Enabled: auth0.Bool(true),
+			t.Run("Enable", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.DUO.Enable(false)
+
+				err := m.Guardian.MultiFactor.DUO.Enable(true)
+				if err != nil {
+					t.Error(err)
+				}
+
+				mfa, _ := m.Guardian.MultiFactor.List()
+				t.Logf("%v\n", mfa)
+			})
 		})
-		if err != nil {
-			t.Error(err)
-		}
-		gf, _ := m.Guardian.ListFactors()
-		t.Logf("%v\n", gf)
-	})
 
-	t.Run("GetSmsTemplate", func(t *testing.T) {
-		st, err := m.Guardian.GetSmsTemplate()
-		if err != nil {
-			t.Error(err)
-		}
-		t.Logf("%v\n", st)
-	})
+		t.Run("OTP", func(t *testing.T) {
 
-	t.Run("UpdateSmsTemplate", func(t *testing.T) {
-		err := m.Guardian.UpdateSmsTemplate(&GuardianSmsTemplate{
-			EnrollmentMessage:   auth0.String("Test {{code}} for {{tenant.friendly_name}}"),
-			VerificationMessage: auth0.String("Test {{code}} for {{tenant.friendly_name}}"),
+			t.Run("Enable", func(t *testing.T) {
+				defer m.Guardian.MultiFactor.OTP.Enable(false)
+
+				err := m.Guardian.MultiFactor.OTP.Enable(true)
+				if err != nil {
+					t.Error(err)
+				}
+
+				mfa, _ := m.Guardian.MultiFactor.List()
+				t.Logf("%v\n", mfa)
+			})
 		})
-		if err != nil {
-			t.Error(err)
-		}
-		st, _ := m.Guardian.GetSmsTemplate()
-		t.Logf("%v\n", st)
-	})
-
-	t.Run("GetPushNotificationSnsConfig", func(t *testing.T) {
-		sc, err := m.Guardian.GetPushNotificationSnsConfig()
-		if err != nil {
-			t.Error(err)
-		}
-		t.Logf("%v\n", sc)
-	})
-
-	t.Run("UpdatePushNotificationSnsConfig", func(t *testing.T) {
-		err := m.Guardian.UpdatePushNotificationSnsConfig(&GuardianPushNotificationSnsConfig{
-			AwsAccessKeyID:                auth0.String("test"),
-			AwsSecretAccessKeyID:          auth0.String("test_secret"),
-			AwsRegion:                     auth0.String("us-west-1"),
-			SnsApnsPlatformApplicationArn: auth0.String("test_arn"),
-			SnsGcmPlatformApplicationArn:  auth0.String("test_arn"),
-		})
-		if err != nil {
-			t.Error(err)
-		}
-		sc, _ := m.Guardian.GetPushNotificationSnsConfig()
-		t.Logf("%v\n", sc)
-	})
-
-	t.Run("GetSmsTwilioConfig", func(t *testing.T) {
-		tc, err := m.Guardian.GetSmsTwilioConfig()
-		if err != nil {
-			t.Error(err)
-		}
-		t.Logf("%v\n", tc)
-	})
-
-	t.Run("UpdateSmsTwilioConfig", func(t *testing.T) {
-		err := m.Guardian.UpdateSmsTwilioConfig(&GuardianSmsTwilioConfig{
-			From:      auth0.String("123456789"),
-			AuthToken: auth0.String("test_token"),
-			Sid:       auth0.String("test_sid"),
-		})
-		if err != nil {
-			t.Error(err)
-		}
-		tc, _ := m.Guardian.GetSmsTwilioConfig()
-		t.Logf("%v\n", tc)
 	})
 }
