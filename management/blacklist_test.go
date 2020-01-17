dabdcc471b3d9dc8794904795cc16302c980e65b
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2020-01-17 13:42:52 +0200

    update blacklists

--- management/blacklist_test.go
+++ management/blacklist_test.go
@@ -17,14 +17,6 @@ func TestBlacklist(t *testing.T) {
 	}
 	defer m.Client.Delete(auth0.StringValue(c.ClientID))
 
-	t.Run("List", func(t *testing.T) {
-		bl, err := m.Blacklist.List()
-		if err != nil {
-			t.Error(err)
-		}
-		t.Logf("%v\n", bl)
-	})
-
 	t.Run("Create", func(t *testing.T) {
 		err := m.Blacklist.Create(&BlacklistToken{
 			Audience: auth0.StringValue(c.ClientID),
@@ -39,6 +31,14 @@ func TestBlacklist(t *testing.T) {
 		}
 		if len(bl) == 0 {
 			t.Error("unexpected output; blacklist should not be empty")
+		}
+		t.Logf("%v\n", bl)
+	})
+
+	t.Run("List", func(t *testing.T) {
+		bl, err := m.Blacklist.List(Parameter("aud", auth0.StringValue(c.ClientID)))
+		if err != nil {
+			t.Error(err)
 		}
 		t.Logf("%v\n", bl)
 	})
