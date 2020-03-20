ad50ad87abe23327f72efa42d1402ba2ce2514e8
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2020-03-20 16:02:09 +0100

    make some ad scopes optional

--- management/connection.go
+++ management/connection.go
@@ -498,9 +498,9 @@ type ConnectionOptionsAD struct {
 	LogoURL       *string       `json:"icon_url,omitempty"`
 	IPs           []interface{} `json:"ips"`
 
-	CertAuth             *bool `json:"certAuth"`
-	Kerberos             *bool `json:"kerberos"`
-	DisableCache         *bool `json:"disable_cache"`
+	CertAuth             *bool `json:"certAuth,omitempty"`
+	Kerberos             *bool `json:"kerberos,omitempty"`
+	DisableCache         *bool `json:"disable_cache,omitempty"`
 	BruteForceProtection *bool `json:"brute_force_protection,omitempty"`
 
 	SetUserAttributes *string `json:"set_user_root_attributes,omitempty"`
