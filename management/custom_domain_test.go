ee8a24225cfa69c297d7b4878f1454b9bb196f68
Simon Claessens <gagalago@gmail.com>
2020-11-24 15:39:19 +0100

    Revert "add custom domain setup"
    This reverts commit 5a709fbf97440ba966299e81e77e9eb8636f4295.

--- management/custom_domain_test.go
+++ management/custom_domain_test.go
@@ -18,20 +18,14 @@ func TestCustomDomain(t *testing.T) {
 
 	var err error
 
-	// setup
-	c, err = m.CustomDomain.Read(c.GetID())
-	if err != nil {
-		if err, ok := err.(Error); ok && err.Status() == http.StatusNotFound {
-			t.Skip(err)
-		}
-	} else {
-		m.CustomDomain.Delete(c.GetID())
-	}
-
 	t.Run("Create", func(t *testing.T) {
 		err = m.CustomDomain.Create(c)
 		if err != nil {
-			t.Error(err)
+			if err, ok := err.(Error); ok && err.Status() == http.StatusForbidden {
+				t.Skip(err)
+			} else {
+				t.Error(err)
+			}
 		}
 		t.Logf("%v\n", c)
 	})
@@ -39,7 +33,11 @@ func TestCustomDomain(t *testing.T) {
 	t.Run("Read", func(t *testing.T) {
 		c, err = m.CustomDomain.Read(c.GetID())
 		if err != nil {
-			t.Error(err)
+			if err, ok := err.(Error); ok && err.Status() == http.StatusNotFound {
+				t.Skip(err)
+			} else {
+				t.Error(err)
+			}
 		}
 		t.Logf("%v\n", c)
 	})
@@ -47,7 +45,11 @@ func TestCustomDomain(t *testing.T) {
 	t.Run("Verify", func(t *testing.T) {
 		c, err := m.CustomDomain.Verify(c.GetID())
 		if err != nil {
-			t.Error(err)
+			if err, ok := err.(Error); ok && err.Status() == http.StatusNotFound {
+				t.Skip(err)
+			} else {
+				t.Error(err)
+			}
 		}
 		t.Logf("%v\n", c)
 	})
@@ -55,7 +57,11 @@ func TestCustomDomain(t *testing.T) {
 	t.Run("Delete", func(t *testing.T) {
 		err = m.CustomDomain.Delete(c.GetID())
 		if err != nil {
-			t.Error(err)
+			if err, ok := err.(Error); ok && err.Status() == http.StatusNotFound {
+				t.Skip(err)
+			} else {
+				t.Error(err)
+			}
 		}
 	})
 }
