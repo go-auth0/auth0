db5dcf0987369b1b999cab68baa1a902d7027cc0
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2021-07-28 12:19:33 +0200

    pass empty Role{} to delete

--- management/role.go
+++ management/role.go
@@ -69,7 +69,7 @@ func (m *RoleManager) Update(id string, r *Role, opts ...RequestOption) (err err
 //
 // See: https://auth0.com/docs/api/management/v2#!/Roles/delete_roles_by_id
 func (m *RoleManager) Delete(id string, opts ...RequestOption) (err error) {
-	return m.Request("DELETE", m.URI("roles", id), nil, opts...)
+	return m.Request("DELETE", m.URI("roles", id), &Role{}, opts...)
 }
 
 // List all roles that can be assigned to users or groups.
