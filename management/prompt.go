33466b1ca8961398ab74eb47fdb28359bc15609b
Rita Zerrizuela <zeta@widcket.com>
2021-10-06 01:21:58 -0300

    Add support for custom prompt text

--- management/prompt.go
+++ management/prompt.go
@@ -1,5 +1,7 @@
 package management
 
+import "gopkg.in/auth0.v5"
+
 type Prompt struct {
 	// Which login experience to use. Can be `new` or `classic`.
 	UniversalLoginExperience string `json:"universal_login_experience,omitempty"`
@@ -10,6 +12,10 @@ type Prompt struct {
 
 type PromptManager struct {
 	*Management
+}
+
+type promptCustomText struct {
+	Text *string
 }
 
 func newPromptManager(m *Management) *PromptManager {
@@ -29,4 +35,40 @@ func (m *PromptManager) Read(opts ...RequestOption) (p *Prompt, err error) {
 // See: https://auth0.com/docs/api/management/v2#!/Prompts/patch_prompts
 func (m *PromptManager) Update(p *Prompt, opts ...RequestOption) error {
 	return m.Request("PATCH", m.URI("prompts"), p, opts...)
+}
+
+// MarshalJSON is a custom serializer for the promptCustomText type.
+//
+// We have to use one to avoid creating a new Request method that does not JSON-encode the request body.
+func (t *promptCustomText) MarshalJSON() ([]byte, error) {
+	return []byte(auth0.StringValue(t.Text)), nil
+}
+
+// UnmarshalJSON is a custom deserializer for the promptCustomText type.
+//
+// We have to use one to avoid creating a new Request method that does not JSON-decode the response body.
+func (t *promptCustomText) UnmarshalJSON(b []byte) error {
+	t.Text = auth0.String(string(b))
+	return nil
+}
+
+// Retrieve custom text for a specific prompt and language.
+//
+// See: https://auth0.com/docs/api/management/v2#!/Prompts/get_custom_text_by_language
+func (m *PromptManager) CustomText(p string, l string, opts ...RequestOption) (t *string, err error) {
+	var r *promptCustomText
+	err = m.Request("GET", m.URI("prompts", p, "custom-text", l), &r, opts...)
+	t = r.Text
+	return
+}
+
+// Set custom text for a specific prompt. Existing texts will be overwritten.
+//
+// See: https://auth0.com/docs/api/management/v2#!/Prompts/put_custom_text_by_language
+func (m *PromptManager) SetCustomText(p string, l string, b *string, opts ...RequestOption) (err error) {
+	r := &promptCustomText{Text: b}
+	err = m.Request("PUT", m.URI("prompts", p, "custom-text", l), r, opts...)
+	//lint:ignore SA4006 because the only purpose of b is to  be assigned to
+	b = r.Text
+	return
 }
--- management/prompt_test.go
+++ management/prompt_test.go
@@ -1,9 +1,10 @@
 package management
 
 import (
+	"testing"
+
 	"gopkg.in/auth0.v5"
 	"gopkg.in/auth0.v5/internal/testing/expect"
-	"testing"
 )
 
 func TestPrompt(t *testing.T) {
@@ -17,35 +18,73 @@ func TestPrompt(t *testing.T) {
 		}
 	})
 
-	// update to the new identifier first experience
-	err := m.Prompt.Update(&Prompt{
-		UniversalLoginExperience: "new",
-		IdentifierFirst:          auth0.Bool(true),
+	t.Run("Update to the new identifier first experience", func(t *testing.T) {
+		err := m.Prompt.Update(&Prompt{
+			UniversalLoginExperience: "new",
+			IdentifierFirst:          auth0.Bool(true),
+		})
+		if err != nil {
+			t.Error(err)
+		}
+
+		ps, err := m.Prompt.Read()
+		if err != nil {
+			t.Error(err)
+		}
+		expect.Expect(t, ps.UniversalLoginExperience, "new")
+		expect.Expect(t, ps.IdentifierFirst, auth0.Bool(true))
 	})
-	if err != nil {
-		t.Error(err)
-	}
 
-	ps, err := m.Prompt.Read()
-	if err != nil {
-		t.Error(err)
-	}
-	expect.Expect(t, ps.UniversalLoginExperience, "new")
-	expect.Expect(t, ps.IdentifierFirst, auth0.Bool(true))
+	t.Run("Update to the classic non identifier first experience", func(t *testing.T) {
+		err := m.Prompt.Update(&Prompt{
+			UniversalLoginExperience: "classic",
+			IdentifierFirst:          auth0.Bool(false),
+		})
+		if err != nil {
+			t.Error(err)
+		}
 
-	// update to the classic non identifier first experience
-	err = m.Prompt.Update(&Prompt{
-		UniversalLoginExperience: "classic",
-		IdentifierFirst:          auth0.Bool(false),
+		ps, err := m.Prompt.Read()
+		if err != nil {
+			t.Error(err)
+		}
+		expect.Expect(t, ps.UniversalLoginExperience, "classic")
+		expect.Expect(t, ps.IdentifierFirst, auth0.Bool(false))
 	})
-	if err != nil {
-		t.Error(err)
-	}
+}
 
-	ps, err = m.Prompt.Read()
-	if err != nil {
-		t.Error(err)
-	}
-	expect.Expect(t, ps.UniversalLoginExperience, "classic")
-	expect.Expect(t, ps.IdentifierFirst, auth0.Bool(false))
+func TestPromptCustomText(t *testing.T) {
+	t.Cleanup(func() {
+		prompt := "login"
+		lang := "en"
+		body := "{}"
+
+		err := m.Prompt.SetCustomText(prompt, lang, &body)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Retrieve custom text", func(t *testing.T) {
+		prompt := "login"
+		lang := "en"
+
+		texts, err := m.Prompt.CustomText(prompt, lang)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Logf("%v\n", texts)
+	})
+
+	t.Run("Set custom text", func(t *testing.T) {
+		prompt := "login"
+		lang := "en"
+		body := `{ "login": { "title": "Welcome" } }`
+
+		err := m.Prompt.SetCustomText(prompt, lang, &body)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Logf("%v\n", body)
+	})
 }
